051581d0a5facc3e3f94c35e4d5b7956fc3bb958
Brenda <waiyegobrenda99@gmail.com>
2022-08-20 16:27:55 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution in this file!
+var customerName = "bob";
+function upperCaseCustomerName() {
+    return customerName=customerName.toUpperCase()
+}
+function setBestCustomer() {
+     bestCustomer="not bob"
+    console.log (bestCustomer)
+}
+ 
+function overwriteBestCustomer() {
+    return bestCustomer="maybe bob"
+}
+
+const leastFavoriteCustomer="kinuthia"
+function changeLeastFavoriteCustomer() {
+   return leastFavoriteCustomer="njambi" 
+}
